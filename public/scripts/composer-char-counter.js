16be33cb64bbe5bfbab3a0be3df956d77024d655
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-07-17 15:51:46 -0600

    Implement a way to reduce counter when typing

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log(document)
+
+  $("#tweet-text").on('input', function() {
+    const userInput = $(this).val().length;
+    const counter = $(this).next().children('.counter');
+    
+    const availableText = 140 - userInput;
+
+    counter.text(availableText);
+
+    if (availableText < 0) {
+      counter.css('color', 'red')
+    } else {
+      counter.css('color', '#545149')
+    }
+    
+  });   
+});
